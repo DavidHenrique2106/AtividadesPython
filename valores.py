f6137283cfd4b206f5c98abd93bf7e8b031ab750
DavidHenrique2106 <dh210605@gmail.com>
2023-09-18 23:02:54 -0300

    Valores

---
+++ valores.py
@@ -0,0 +1,9 @@
+a=int(float(input("Digite o valor de A -> ")))
+b=int(float(input("Digite o valor de B -> ")))
+c=int(float(input("Digite o valor de C -> ")))
+
+if a + b > c:
+    print("Os valores de A e B juntos é maior que o de C!")
+
+else:
+    print("Os valores de A e B juntos é menor que o de C!")
